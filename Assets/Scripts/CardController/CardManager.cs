f391304b00ebb9e98d8e7c7a6420ac9639233358
s9716903 <118097638+s9716903@users.noreply.github.com>
2023-04-01 14:09:21 +0800

    修改卡片資料判定

--- Assets/Scripts/CardController/CardManager.cs
+++ Assets/Scripts/CardController/CardManager.cs
@@ -5,44 +5,68 @@ using UnityEngine;
 public class CardManager : MonoBehaviour
 {
     public CardValueManager cardvaluemanager; //卡片資料
+
+    private bool isUseThisCard;
     private bool isCardUp; //卡片是否為正位置
+    private int[] PlayerZone = new int[] { 0, 1, 2, 3, 4 }; //攻擊範圍
+   
     public bool candraw; //用完是否可抽牌
-    public bool[] AttackZone; //攻擊範圍
+    public int[] CanAttack = new int[5]; //可攻擊位置
+
+    [Header("CardValue")]
     public int Type; //種類
     public string Name; //名字
     public int Value; //數值
+
     private void OnEnable()
     {
-        AttackZone = new bool[5];
         isCardUp = true;
+        isUseThisCard = false;
     }
     // Update is called once per frame
     void Update()
     {
+        //卡片正位置
         if (isCardUp == true)
         {
             candraw = cardvaluemanager.cardvalue.candraw;
-            AttackZone = cardvaluemanager.cardvalue.AttackZone;
+            CanAttack = cardvaluemanager.cardvalue.CanAttack;
             Type = cardvaluemanager.cardvalue.Type;
             Name = cardvaluemanager.cardvalue.Name;
             Value = cardvaluemanager.cardvalue.Value;
         }
+
+        //卡片逆位置
         if (isCardUp == false)
         {
             candraw = cardvaluemanager.cardvalue.candraw2;
-            AttackZone = cardvaluemanager.cardvalue.AttackZone2;
+            CanAttack = cardvaluemanager.cardvalue.CanAttack2;
             Type = cardvaluemanager.cardvalue.Type2;
             Name = cardvaluemanager.cardvalue.Name2;
             Value = cardvaluemanager.cardvalue.Value2;
         }
 
+        //滑鼠左鍵卡片時
         if (Input.GetMouseButtonDown(0))
-        { 
-        
+        {
+            isUseThisCard = !isUseThisCard;
         }
+
+        //滑鼠右鍵卡片時
         if (Input.GetMouseButtonDown(1))
         {
             isCardUp = !isCardUp;
+        }
+    }
+    private void UseCard()
+    {
+        if (isUseThisCard)
+        {
+            
+        }
+        else
+        { 
+            
         }
     }
 }
--- Assets/Scripts/CardController/CardValueManager.cs
+++ Assets/Scripts/CardController/CardValueManager.cs
@@ -5,19 +5,23 @@ using UnityEngine;
 [CreateAssetMenu]
 public class CardValueManager : ScriptableObject
 {
-    public CardValue cardvalue; //序列化CardValue資料並轉成ScriptableObject
+    //序列化CardValue資料並轉成ScriptableObject
+    public CardValue cardvalue; 
 }
 [System.Serializable]
 public class CardValue
 {
-    public bool candraw; //用完是否可抽牌(上半)
-    public int Type; //種類(上半)
-    public string Name; //名字(上半)
-    public int Value; //數值(上半)
-    public bool[] AttackZone;
-    public bool candraw2; //用完是否可抽牌(下半)
-    public int Type2; //種類(下半)
-    public string Name2; //名字(下半)
-    public int Value2; //數值(下半)
-    public bool[] AttackZone2;
+    //卡片上半資料
+    public bool candraw; //用完是否可抽牌
+    public int Type; //種類
+    public string Name; //名字
+    public int Value; //數值
+    public int[] CanAttack = new int[5]; //可攻擊位置
+
+    //卡片下半資料
+    public bool candraw2;
+    public int Type2;
+    public string Name2;
+    public int Value2;
+    public int[] CanAttack2 = new int[5];
 }
